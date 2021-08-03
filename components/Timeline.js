e8fe23bda7b3bef845dab4234df29f59120732ba
Manav <18483618+Manav-SM@users.noreply.github.com>
2021-08-03 22:14:02 +0530

    edited timeline page

--- components/Timeline.js
+++ components/Timeline.js
@@ -41,10 +41,16 @@ export default function Timeline() {
 
   return (
     <>
-      <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-        Timeline
-      </h1>
-      <Divider />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Timeline
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            View the highlights of my life
+          </p>
+        </div>
+      </div>
       <Year>2021</Year>
       <ul>
         <Step title="Title">
